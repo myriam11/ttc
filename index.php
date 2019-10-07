e7b7caf2e136f4c30c6898a569d5781022ef0f98
Myriam <myriamb2010@yahoo.fr>
2019-10-07 15:18:22 +0200

    second commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Trouves Ta Caisse</title>
+</head>
+
+<body>
+    <header>
+         <nav>
+            <ul>
+               <li><a href="#">Vendre</li>
+               <li><a href="#">Rechercher</li>
+               <li><a href="#">Annuaires des Professionnels</li>
+               <li><a href="#">Déposer une annonce</li>
+             </ul>
+         </nav>
+    </header>
+
+    
+</body>
+
+</html>
