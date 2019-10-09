df7a9fd587b18c741c876d37239293396f6d7463
Myriam <myriamb2010@yahoo.fr>
2019-10-09 16:52:51 +0200

    AJOUT LISTE MARQUES

--- index.php
+++ index.php
@@ -12,29 +12,124 @@
     <header>
                 <h1>Trouves Ta Caisse</h1>
          <nav>
-            <ul>
-                <li><a href="#">Accueil</li>
+            <ul><li><a href="#">Accueil</li>
                <li><a href="#">Vendre</li>
                <li><a href="#">Rechercher</li>
                <li><a href="#">Annuaires des Professionnels</li>
-               
-             </ul>
+            </ul>
          </nav>
     </header>
 
-    <main>
+            <main>
+              
+                <section>
+                       
 
-            <img src="assets/images/audi.jpg" alt="cars" >
+                    <div id="box">
+                    <form>
+                         <div class="form-row align-items-center">
+                             <div class="col-auto my-1">
+                             <!-- <label class="mr-sm-2" for="inlineFormCustomSelect">Preference</label>-->
+                             <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                                     <option selected>Occasion...</option>
+                                    <option value="1">Occasion</option>
+                                    <option value="2">Neuf</option>
+                            </select>
+                         </div>
+
+
+                         <div class="form-row align-items-center">
+                             <div class="col-auto my-1">
+                             <!-- <label class="mr-sm-2" for="inlineFormCustomSelect">Preference</label>-->
+                             <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                                     <option selected>Marque...</option>
+                                    <option value="1">Renault</option>
+                                    <option value="2">Peugeot</option>
+                                    <option value="3">Citroen</option>
+                                    <option value="4">Volkswagen</option>
+                                    <option value="5">BMW</option>
+                                    <option value="6">Audi</option>
+                                    <option value="7">Alfa Romeo</option>
+                                    <option value="8">Austin</option>
+                                    <option value="9">Bentley</option>
+                                    <option value="10">Chevrolet</option>
+                                    <option value="11">Ferrari</option>
+                                    <option value="12">Hyundai</option>
+                                    <option value="13">kia</option>
+                                    <option value="14">Lancia</option>
+                                    <option value="15">Land Rover</option>
+                                    <option value="16">Lexus</option>
+                                    <option value="17">Mazda</option>
+                                    <option value="18">Mercedes</option>
+                                    <option value="19">Mini</option>
+                                    <option value="20">Mitsubishi</option>
+                                    <option value="21">Nissan</option>
+                                    <option value="22">Piaggio</option>
+                                    <option value="23">Porsche</option>
+                                    <option value="24">Seat</option>
+                                    <option value="25">Skoda</option>
+                                    <option value="26">Subaru</option>
+                                    <option value="27">Toyota</option>
+                                    <option value="28">Volvo</option>
+
+                            </select>
+                         </div>
+
+                         <div class="form-row align-items-center">
+                             <div class="col-auto my-1">
+                             <!-- <label class="mr-sm-2" for="inlineFormCustomSelect">Preference</label>-->
+                             <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                                     <option selected>Modèle...</option>
+                                    <option value="1">Renault</option>
+                                    <option value="2">Peugeot</option>
+                                 <option value="3">Citroen</option>
+                            </select>
+                         </div>
 
 
 
-    </main>
-    
+
+
+
+
+
+
+                      
+                        </div>
+                        <div class="col-auto my-1">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </div>
+                    </form>
+                     
+                    </div>
+                </section>
+
+
+
+
+
+
+
+
+
+
+            </main>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                 
-
-
-    
-
-
     </body>
 </html>
