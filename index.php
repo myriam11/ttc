25ee122505af33f93baeb4e5b1b8355e422085b8
Tadikira <kiiraomar17@gmail.com>
2019-10-17 10:06:12 +0200

    barre nav

--- index.php
+++ index.php
@@ -1,197 +1,144 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
+  <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="assets/style.css">
     <title>Trouves Ta Caisse</title>
+<head>
+  	<nav>
+			<div id="logo">
+				<h1>Trouves Ta Caisse</h1> 
+			</div>
+
+			<ul>
+				<li>Accueil</li>
+				<li>Vendre</li>
+				<li>Rechercher</li>
+				<li id="button"><button id="buy">Annuaires des Professionnels</button></li>
+			</ul>
+		</nav>
 </head>
-
 <body>
-    <header>
-                <h1>Trouves Ta Caisse</h1>
-         <nav>
-            <ul><li><a href="#">Accueil</li>
-               <li><a href="#">Vendre</li>
-               <li><a href="#">Rechercher</li>
-               <li><a href="#">Annuaires des Professionnels</li>
-            </ul>
-         </nav>
-    </header>
+    <main>
+   
+    <aside id="login">
+  <form name="form1">
+    
+    <select name="champ1" >
+      <option selected>Occasion...</option>
+       <option value="1">Occasion</option>
+        <option value="2">Neuf</option>
+        
+     </select>
+     
+     <select name="champ2" >
+        <option>Marque...</option>
+        <option value="1">Renault</option>
+        <option value="2">Peugeot</option>
+        <option value="3">Citroen</option>
+        <option value="4">Volkswagen</option>
+        <option value="5">BMW</option>
+        <option value="6">Audi</option>
+        <option value="7">Alfa Romeo</option>
+        <option value="8">Austin</option>
+        <option value="9">Bentley</option>
+        <option value="10">Chevrolet</option>
+        <option value="11">Ferrari</option>
+        <option value="12">Hyundai</option>
+        <option value="13">kia</option>
+        <option value="14">Lancia</option>
+        <option value="15">Land Rover</option>
+        <option value="16">Lexus</option>
+        <option value="17">Mazda</option>
+        <option value="18">Mercedes</option>
+        <option value="19">Mini</option>
+        <option value="20">Mitsubishi</option>
+        <option value="21">Nissan</option>
+        <option value="22">Piaggio</option>
+        <option value="23">Porsche</option>
+        <option value="24">Seat</option>
+        <option value="25">Skoda</option>
+        <option value="26">Subaru</option>
+        <option value="27">Toyota</option>
+        <option value="28">Volvo</option>
+     </select> 
+     
+     <select name="champ3" >
+        <option selected>Modèle...</option>
+        <option value="1">Clio</option>
+        <option value="2">Captur</option>
+        <option value="3">208</option>
+        <option value="4">308</option>
+        <option value="5">Giulietta</option>
+        <option value="6">A1</option>
+        <option value="7">A2</option>
+        <option value="8">A3</option>
+        <option value="9">Q2</option>
+        <option value="10">Q3</option>
+        <option value="11">Q5</option>
+        <option value="12">R8</option>
+        <option value="13">Mini</option>
+        <option value="14">Lancia</option>
+        <option value="15">Land Rover</option>
+        <option value="16">Lexus</option>
+        <option value="17">Mazda</option>
+        <option value="18">Mercedes</option>
+        <option value="19">Mini</option>
+        <option value="20">Mitsubishi</option>
+        <option value="21">Nissan</option>
+        <option value="22">Piaggio</option>
+        <option value="23">Porsche</option>
+        <option value="24">Seat</option>
+        <option value="25">Skoda</option>
+        <option value="26">Subaru</option>
+        <option value="27">Toyota</option>
+        <option value="28">Volvo</option>
+     </select>
+     
+     <form name="champ4" >
+      <input type="number" value="" placeholder="Prix max €" pattern="\d*"" max="9999999">
+     </select> 
+     
+     <select name="champ5" >
+        <option selected>Carburant</option>
+        <option>Diesel</option>
+        <option>Essence</option>
+        <option>Electrique</option>
+        <option>Hybride</option>
+     </select>
+  
+    <select name="champ6" >
+       <option selected>Régions</option>
+       <option>Auvergne-Rhône-Alpes</option>
+       <option>Bourgongne-Franche-Comté</option>
+       <option>bretagne</option>
+       <option>Centre-Val de Loire</option>
+       <option>Corse</option>
+       <option>Grand Est</option>
+       <option>Hauts-de-france</option>
+       <option>Île-de-France</option>
+       <option>Normandie</option>
+       <option>Nouvelle-Aquitaine</option>
+       <option>Occitanie</option>
+       <option>Pays de la Loire</option>
+       <option>Provence-Alpes-Côte d"Azur</option>
+    </select>
 
-            <main>
-              
-                <section id="audi">
-                
-
-                    <div id="box">
-                    <form>
-                         <div class="form-row align-items-center">
-                             <div class="col-auto my-1">
-                             <!-- <label class="mr-sm-2" for="inlineFormCustomSelect">Preference</label>-->
-                             <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                                     <option selected>Occasion...</option>
-                                    <option value="1">Occasion</option>
-                                    <option value="2">Neuf</option>
-                            </select>
-                         </div>
-
-
-                         <div class="form-row align-items-center">
-                             <div class="col-auto my-1">
-                             <!-- <label class="mr-sm-2" for="inlineFormCustomSelect">Preference</label>-->
-                             <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                                     <option selected>Marque...</option>
-                                    <option value="1">Renault</option>
-                                    <option value="2">Peugeot</option>
-                                    <option value="3">Citroen</option>
-                                    <option value="4">Volkswagen</option>
-                                    <option value="5">BMW</option>
-                                    <option value="6">Audi</option>
-                                    <option value="7">Alfa Romeo</option>
-                                    <option value="8">Austin</option>
-                                    <option value="9">Bentley</option>
-                                    <option value="10">Chevrolet</option>
-                                    <option value="11">Ferrari</option>
-                                    <option value="12">Hyundai</option>
-                                    <option value="13">kia</option>
-                                    <option value="14">Lancia</option>
-                                    <option value="15">Land Rover</option>
-                                    <option value="16">Lexus</option>
-                                    <option value="17">Mazda</option>
-                                    <option value="18">Mercedes</option>
-                                    <option value="19">Mini</option>
-                                    <option value="20">Mitsubishi</option>
-                                    <option value="21">Nissan</option>
-                                    <option value="22">Piaggio</option>
-                                    <option value="23">Porsche</option>
-                                    <option value="24">Seat</option>
-                                    <option value="25">Skoda</option>
-                                    <option value="26">Subaru</option>
-                                    <option value="27">Toyota</option>
-                                    <option value="28">Volvo</option>
-
-                            </select>
-                         </div>
-
-                         <div class="form-row align-items-center">
-                             <div class="col-auto my-1">
-                             <!-- <label class="mr-sm-2" for="inlineFormCustomSelect">Preference</label>-->
-                             <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                                     <option selected>Modèle...</option>
-                                     <option value="1">Clio</option>
-                                    <option value="2">Captur</option>
-                                    <option value="3">208</option>
-                                    <option value="4">308</option>
-                                    <option value="5">Giulietta</option>
-                                    <option value="6">A1</option>
-                                    <option value="7">A2</option>
-                                    <option value="8">A3</option>
-                                    <option value="9">Q2</option>
-                                    <option value="10">Q3</option>
-                                    <option value="11">Q5</option>
-                                    <option value="12">R8</option>
-                                    <option value="13">Mini</option>
-                                    <option value="14">Lancia</option>
-                                    <option value="15">Land Rover</option>
-                                    <option value="16">Lexus</option>
-                                    <option value="17">Mazda</option>
-                                    <option value="18">Mercedes</option>
-                                    <option value="19">Mini</option>
-                                    <option value="20">Mitsubishi</option>
-                                    <option value="21">Nissan</option>
-                                    <option value="22">Piaggio</option>
-                                    <option value="23">Porsche</option>
-                                    <option value="24">Seat</option>
-                                    <option value="25">Skoda</option>
-                                    <option value="26">Subaru</option>
-                                    <option value="27">Toyota</option>
-                                    <option value="28">Volvo</option>
-                            </select>
-                         </div>
-
-
-                         <div class="form-row align-items-center">
-                             <div class="col-auto my-1">
-                             <!-- <label class="mr-sm-2" for="inlineFormCustomSelect">Preference</label>-->
-                             <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                                     <option selected>Prix max €</option>
-                                   
-                            </select>
-                         </div>
+      <button type="submit" class="btn btn-primary">annonces</button>
+</form>
+</aside>
 
 
-                         <div class="form-row align-items-center">
-                             <div class="col-auto my-1">
-                             <!-- <label class="mr-sm-2" for="inlineFormCustomSelect">Preference</label>-->
-                             <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                                     <option selected>Carburant</option>
-                                    <option value="1">Diesel</option>
-                                    <option value="2">Essence</option>
-                                    <option value="3">Electrique</option>
-                                    <option value="4">Hybride</option>
-                            </select>
-                         </div>
+    
+   
+      <h1>Suite du site</h1>
+   
+</main>
+</body>
+<footer>
 
-                         <div class="form-row align-items-center">
-                             <div class="col-auto my-1">
-                             <!-- <label class="mr-sm-2" for="inlineFormCustomSelect">Preference</label>-->
-                             <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                                     <option selected>Régions</option>
-                                    <option value="1">Auvergne-Rhône-Alpes</option>
-                                    <option value="2">Bourgongne-Franche-Comté</option>
-                                    <option value="3">bretagne</option>
-                                    <option value="4">Centre-Val de Loire</option>
-                                    <option value="5">Corse</option>
-                                    <option value="6">Grand Est</option>
-                                    <option value="7">Hauts-de-france</option>
-                                    <option value="8">Île-de-France</option>
-                                    <option value="9">Normandie</option>
-                                    <option value="10">Nouvelle-Aquitaine</option>
-                                    <option value="11">Occitanie</option>
-                                    <option value="12">Pays de la Loire</option>
-                                    <option value="13">Provence-Alpes-Côte d'Azur</option>
-                                </select>
-                            </div>
+</footer>
 
 
-                      
-                            </div>
-                            <div class="col-auto my-1">
-                            <button type="submit" class="btn btn-primary">annonces</button>
-                            </div>
-                        </div>
-                        </form>
-                     
-                    </div>
-                </section>
-
-
-
-
-
-
-
-
-
-
-            </main>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                
-    </body>
 </html>
